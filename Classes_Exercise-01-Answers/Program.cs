dee953f1b20209217fd55de7375a21baf365940d
johndward01 <johndward01@gmail.com>
2021-08-11 13:23:31 -0500

    Add project files.

---
+++ Classes_Exercise-01-Answers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Classes_Exercise_01_Answers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

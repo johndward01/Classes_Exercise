5ca920ce2bfb8302f11009da52b18b35ac69cc45
johndward01 <johndward01@gmail.com>
2021-08-11 21:44:54 -0500

    main init

--- Classes_Exercise-01-Answers/Program.cs
+++ Classes_Exercise-01-Answers/Program.cs
@@ -6,7 +6,63 @@ namespace Classes_Exercise_01_Answers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region// TODO: Create a public Employee class
+
+            // Inside of the Employee class:
+            // TODO: Create a private field _password of type string
+
+            // TODO: Create a public FirstName property of type string
+            // TODO: Create a public MiddleInitial property of type char
+            // TODO: Create a public LastName property of type string
+            // TODO: Create a public EmailAddress property of type string
+            // TODO: Create a public PhoneNumber property of type string
+            // TODO: Create a public Title property of type string
+            // TODO: Create a public DateOfBirth property of type DateTime
+
+            // TODO: Create a parameterized constructor that sets all of the properties correctly
+            // Thought experiment: (True or False) We can instantiate this class with a default constructor
+            // TODO: false (bc we created a parameterized constructor we overrided the default. So, if we want to be able to use the default we must explicitly type it out)
+
+            // TODO: Create a method that sets the employee's password
+            // TODO: Create a method to display the employee's password
+            // TODO: Create an EmployeeReport() method that takes in an employee and returns all of the props creatively displayed to the console
+
+            #endregion
+
+            #region// TODO: Create a public Product class 
+
+            // TODO: Create a public Product class
+
+            // TODO: Create a private field _productID of type long
+            // TODO: Create a private field _productCount of type int
+
+            // TODO: Create a public Name property of type string
+            // TODO: Create a public Price property of type double
+            // TODO: Create a public CategoryID property of type int
+            // TODO: Create a public OnSale property of type bool
+            // TODO: Create a public StockLevel property of type int
+
+            // TODO: Create a default constructor that increments the _productCount every time a new product is created
+            // TODO: Create a parameterized constructor that does the same thing and initializes all of the properties
+
+            // TODO: Create a method that gets the _productID and displays it creatively
+            // TODO: Create a method that sets the _productID
+            // TODO: Create a method that shows the current product count
+
+
+            #endregion
+
+            // TODO: Create a new employee and give all of its properties values
+
+
+            // TODO: Call all of the methods you created inside of that class
+
+
+            // TODO: Create a new product and give all of its properties values
+
+            // TODO: Call all of the methods you created inside of that class
+
+
         }
     }
 }
